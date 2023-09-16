331eada0ce5ff25783e28f40b5889f18c78bd916
Edward Sun <eedwardsun@gmail.com>
2023-09-15 20:56:02 -0700

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import type { AppProps } from "next/app";
+import {
+  embeddedWallet,
+  paperWallet,
+  smartWallet,
+  ThirdwebProvider,
+} from "@thirdweb-dev/react";
+import "../styles/globals.css";
+import { Goerli } from "@thirdweb-dev/chains";
+
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+export const activeChain = Goerli;
+
+export const smartWalletConfig = smartWallet({
+  factoryAddress: "0x6B1F8bBF4Af9267C0a483Da53BaE1118eadC50C1",
+  gasless: true,
+  personalWallets: [embeddedWallet()],
+});
+
+export const editionDropAddress = "0xb9e2b6C6ce60d074b96DD19498dF2493f9A625B1";
+export const editionDropTokenId = "0";
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider
+      clientId={"8db750c8ae2be3f072ab8828da69a93c"}
+      activeChain={activeChain}
+      supportedWallets={[smartWalletConfig]}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
