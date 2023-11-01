e961f90bf42984ffa8d6f03ef68088ce29b966d6
Edward Sun <eedwardsun@gmail.com>
2023-10-31 22:26:29 -0700

    auth config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import {
   embeddedWallet,
+  localWallet,
   paperWallet,
   smartWallet,
   ThirdwebProvider,
@@ -13,20 +14,24 @@ import { Goerli } from "@thirdweb-dev/chains";
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
 export const activeChain = Goerli;
 
-export const smartWalletConfig = smartWallet({
-  factoryAddress: "0x6B1F8bBF4Af9267C0a483Da53BaE1118eadC50C1",
-  gasless: true,
-  personalWallets: [embeddedWallet()],
-});
+// export const smartWalletConfig = smartWallet({
+//   factoryAddress: "0x6B1F8bBF4Af9267C0a483Da53BaE1118eadC50C1",
+//   gasless: true,
+//   personalWallets: [embeddedWallet()],
+// });
 
 export const editionDropAddress = "0xb9e2b6C6ce60d074b96DD19498dF2493f9A625B1";
 export const editionDropTokenId = "0";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={"8db750c8ae2be3f072ab8828da69a93c"}
+      clientId={"31dce317f0d4b6f5cafa37ab53a2b087"}
       activeChain={activeChain}
-      supportedWallets={[smartWalletConfig]}
+      supportedWallets={[embeddedWallet(), localWallet()]}
+      authConfig={{
+        domain: "https://thirdweb-preview.com",
+        authUrl: `https://api.thirdweb-preview.com/v1/auth`,
+      }}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
